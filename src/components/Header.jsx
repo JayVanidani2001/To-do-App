d3e1caf638df3e9edd73b6500b30360f786ec5e2
Jay Vanidani <jayro@MacBook-Air.local>
2022-07-20 17:45:54 +0530

    Adding project

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ListAltIcon />
+        To-Do List
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
